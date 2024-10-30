ebd537edd3121ca0acf9598e268165fb88168b4c
Tho <lqt5484@gmail.com>
2024-10-30 15:06:37 +0700

    Lần 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 document.querySelector(".logout-link").addEventListener("click", function (e) {
   e.preventDefault();
   if (confirm("Bạn có chắc chắn muốn đăng xuất?")) {
-    window.location.href = "login.html";
+    window.location.href = "index.html";
   }
 });
 
